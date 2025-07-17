cf5c913a4ed5b4190a17e71f31695042998a2bbf
unknown <channelsgamer@gmail.com>
2025-07-17 12:33:26 +0700

    Lab02

---
+++ app/routes/game-Lab02.tsx
@@ -0,0 +1,14 @@
+function MyProfile () {
+return (
+    <div>
+        <p className="text-2xl ">Game Profile</p>
+        <img src="/images/Sung-Jin-Woo-Wallpaper-8.jpeg" className="w-1/6"/>
+        <p className="text-lg">
+        Name: Warit Niyomthai<br />
+        Student ID: 026730491011-5<br />
+        </p>
+    </div>
+)
+}
+
+export default MyProfile
